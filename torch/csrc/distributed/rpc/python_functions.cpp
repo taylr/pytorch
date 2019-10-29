ad139edff46a61a269010abc3a7fc954c97fdad6
Shen Li <cs.shenli@gmail.com>
2019-10-28 18:19:05 -0700

    Support remote for Python UDF in distributed autograd
    ghstack-source-id: 234e876a346125dd7242335a724cad9614e73a3d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/28656

--- torch/csrc/distributed/rpc/python_functions.cpp
+++ torch/csrc/distributed/rpc/python_functions.cpp
@@ -30,6 +30,7 @@ std::shared_ptr<Operator> matchBuiltinOp(
     const py::kwargs& kwargs,
     Stack& stack) {
   Symbol symbol = Symbol::fromQualString(opName);
+
   if (symbol.is_aten()) {
     for (const auto& op : torch::jit::getAllOperatorsFor(symbol)) {
       try {
@@ -37,7 +38,6 @@ std::shared_ptr<Operator> matchBuiltinOp(
         // ``createStackForSchema`` to avoid throwing an error.
         stack = torch::jit::createStackForSchema(
             op->schema(), args, kwargs, c10::nullopt);
-
       } catch (std::runtime_error& e) {
         VLOG(1) << "Couldn't match schema: " << op->schema()
                 << " to args: " << args << " and kwargs: " << kwargs
@@ -163,6 +163,7 @@ std::shared_ptr<FutureMessage> pyRpcPythonUdf(
   auto pythonUDFCall = c10::guts::make_unique<PythonUDFCall>(
       std::vector<char>(pickledPythonUDF.begin(), pickledPythonUDF.end()),
       tensors);
+
   return sendMessageWithAutograd(
       agent, dst, std::move(*pythonUDFCall).toMessage());
 }
@@ -178,13 +179,20 @@ PyRRef pyRemotePythonUdf(
       ctx.getWorkerId() != dst.id_,
       "Does not support creating RRef on self yet.");
   auto userRRef = ctx.createUserRRef<py::object>(dst.id_);
-  auto fm = agent.send(
+
+  auto pythonRemoteCall = c10::guts::make_unique<PythonRemoteCall>(
+      SerializedPyObj(std::move(pickledPythonUDF), std::move(tensors)),
+      userRRef->rrefId().toIValue(),
+      userRRef->forkId().toIValue());
+
+  // set forceGradRecording to true as even if the args does not contain any
+  // tensor, the return value might still contain tensors.
+  auto fm = sendMessageWithAutograd(
+      agent,
       dst,
-      PythonRemoteCall(
-          SerializedPyObj(std::move(pickledPythonUDF), std::move(tensors)),
-          userRRef->rrefId().toIValue(),
-          userRRef->forkId().toIValue())
-          .toMessage());
+      std::move(*pythonRemoteCall).toMessage(),
+      true /*forceGradRecording*/
+  );
 
   ctx.addPendingUser(userRRef->forkId(), userRRef);
   fm->addCallback(finishAcceptUserRRef);
